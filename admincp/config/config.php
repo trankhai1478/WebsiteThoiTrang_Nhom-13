3e20f03a7ed07c64cffd41bf4572d79be772bae4
Khai Tran <trankhai1478@gmail.com>
2023-03-03 21:08:19 +0700

    connectdb

---
+++ admincp/config/config.php
@@ -0,0 +1,10 @@
+<?php
+$mysqli = new mysqli("localhost","root","","web_mysqli");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Kết nối MYSQLi lỗi: " . $mysqli -> connect_error;
+  exit();
+}
+
+?>
